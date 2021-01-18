0f4935b6dd41c03aa3055c068efa217ad56cd610
Aleksandr Mazalov <aleksandr.mazalov@rubius.com>
2021-01-18 16:07:29 +0700

    Небольшие правки.

---
+++ Scripts/Список зарезервированных портов в Windows.bat
@@ -0,0 +1,3 @@
+echo After Windows Update, some ports are reserved by Windows and applications cannot bind to these ports. please check this command
+
+netsh interface ipv4 show excludedportrange protocol=tcp
